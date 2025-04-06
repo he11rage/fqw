fa817dc9e0c75ba76882d76e55dfd2310db5790f
Nikita Grishunin <nikitagrishn@gmail.com>
2025-04-06 13:37:20 +0500

    rm

--- mywebsite/proposal/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.props_home, name='props'),
-    path('create', views.create, name='create'),
-    path('<int:pk>', views.PropsDeleteView.as_view(), name='props-delete')
-]
